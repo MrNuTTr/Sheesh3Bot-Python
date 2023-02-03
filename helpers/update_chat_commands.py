9a686ee8ca81dfe8f9481b6383e80ee75e8c2bbe
MrNuTTr <43860787+MrNuTTr@users.noreply.github.com>
2023-02-03 10:58:47 -0500

    chat command creator

---
+++ helpers/update_chat_commands.py
@@ -0,0 +1,30 @@
+import os
+import requests
+
+# This is a script you keep to yourself and run locally each time 
+# you want to create/update your application's commands
+
+# global commands are cached and only update every hour
+# url = f'https://discord.com/api/v8/applications/{APP_ID}/commands'
+
+# while guild commands update instantly
+# they're much better for testing
+
+BOT_TOKEN = os.environ['DISCORD_BOT_TOKEN']
+APP_ID = os.environ['DISCORD_APP_ID']
+GUILD_ID = os.environ['DISCORD_GUILD_ID']
+
+
+url = f'https://discord.com/api/v8/applications/{APP_ID}/guilds/{GUILD_ID}/commands'
+
+json = {
+  'name': 'chat',
+  'description': 'Test command.',
+  'options': []
+}
+
+response = requests.post(url, headers={
+  'Authorization': f'Bot {BOT_TOKEN}'
+}, json=json)
+
+print(response.json())
