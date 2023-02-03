af4410dfabb3cd46d6030efc8e05d65857041f54
MrNuTTr <43860787+MrNuTTr@users.noreply.github.com>
2023-02-03 10:48:52 -0500

    Adding signature verification

--- function_app.py
+++ function_app.py
@@ -1,30 +1,41 @@
+import json
+import os
 import azure.functions as func
 import logging
 
+from nacl.signing import VerifyKey
+from nacl.exceptions import BadSignatureError
+
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-# Learn more at aka.ms/pythonprogrammingmodel
-
-# Get started by running the following code to create a function using a HTTP trigger.
+public_key = os.environ["DISCORD_PUBLIC_KEY"]
 
 @app.function_name(name="HttpTrigger1")
 @app.route(route="hello")
 def test_function(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
+    logging.info("Python HTTP trigger function processed a request.")
+    
+    try:
+        verify_req(req)
+        
         return func.HttpResponse(
-            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-            status_code=200
+            json.dumps({
+                "type": "1"
+                }),
+            status_code = 200
         )
+    except BadSignatureError:
+        return func.HttpResponse(
+            "Bad signature",
+            status_code = 401
+        )
+    
+def verify_req(req: func.HttpRequest):
+    signature = req.headers['x-signature-ed25519']
+    timestamp = req.headers['x-signature-timestamp']
+    
+    verify_key = VerifyKey(bytes.fromhex(public_key))
+    
+    message = timestamp + json.dump(req.get_body(), separators=(',', ':'))
+    
+    verify_key.verify(message.encode(), signature=bytes.fromhex(signature))
