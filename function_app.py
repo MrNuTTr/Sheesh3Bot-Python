340f55773e5e49ec022080968c082f9d9e3f1e6e
MrNuTTr <43860787+MrNuTTr@users.noreply.github.com>
2023-02-03 14:50:12 -0500

    Added signature verifcation and ping ACK

--- function_app.py
+++ function_app.py
@@ -13,28 +13,72 @@ PUBLIC_KEY = os.environ["DISCORD_PUBLIC_KEY"]
 @app.function_name(name="InteractionsHandler")
 @app.route(route="interactions")
 def test_function(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info("Python HTTP trigger function processed a request.")
-    
-    try:
-        verify_req(req)
+    logging.warning("Received Interaction from Discord")
         
+    try:
+        verify_request(req)
+        
+        body = req.get_json()
+        
+        if body["type"] == 1:
+            logging.warning("Got ping request")
+            return func.HttpResponse(
+                json.dumps({
+                    "type": 1
+                    }),
+                status_code = 200
+            )
+        elif body["type"] == 2:
+            logging.warning("Got a user command request")
+            bro = command_handler(body)
+            logging.warning(bro.get_body())
+            return bro
+        
+        #Unhandled message type
+        logging.error("Unhandled message type")
+        return func.HttpResponse(
+                json.dumps({
+                    "type": 1 
+                    }),
+            )
+        
+    except BadSignatureError:
+        logging.error("Failed to verify signature")
         return func.HttpResponse(
             json.dumps({
-                "type": "1"
+                "Error": "Could not verify interaction signature"
                 }),
-            status_code = 200
-        )
-    except:
-        return func.HttpResponse(
-            "Bad signature",
             status_code = 401
         )
-    
-def verify_req(req: func.HttpRequest):
-    signature = req.headers['x-signature-ed25519']
-    timestamp = req.headers['x-signature-timestamp']
-    body = req.get_body().decode("utf-8")
-    
-    verify_key = VerifyKey(bytes.fromhex(PUBLIC_KEY))
-    
-    verify_key.verify(f"{timestamp}{body}".encode(), signature=bytes.fromhex(signature))
+    except Exception as ex:
+        logging.error(ex)
+        return func.HttpResponse(
+            json.dumps({
+                "Unkown Error": ex 
+                }),
+            status_code = 500
+        )
+
+def command_handler(body) -> func.HttpResponse:
+    return func.HttpResponse(
+        json.dumps({
+            "type": 4,
+            "data": {
+                "content": "Hello, world!"
+            }
+            })
+    )
+
+def verify_request(req: func.HttpRequest):
+    try:
+        signature = req.headers["x-signature-ed25519"]
+        timestamp = req.headers["x-signature-timestamp"]
+        body = req.get_json()
+        
+        message = timestamp + json.dumps(body, separators=(",", ":"))
+        
+        vk = VerifyKey(bytes.fromhex(PUBLIC_KEY))
+        vk.verify(message.encode(), bytes.fromhex(signature))
+
+    except Exception as ex:
+        raise BadSignatureError(ex)
