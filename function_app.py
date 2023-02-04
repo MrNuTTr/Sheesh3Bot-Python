71e790f67efb484240459aee03ede390ae546508
Eli <43860787+MrNuTTr@users.noreply.github.com>
2023-02-03 22:24:08 -0500

    Fixed invalid interaction

--- function_app.py
+++ function_app.py
@@ -2,71 +2,66 @@ import json
 import os
 import azure.functions as func
 import logging
-import requests
 
 from nacl.signing import VerifyKey
 from nacl.exceptions import BadSignatureError
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+app = func.FunctionApp()
 
 PUBLIC_KEY = os.environ["DISCORD_PUBLIC_KEY"]
 
 @app.function_name(name="InteractionsHandler")
-@app.route(route="interactions")
-def test_function(req: func.HttpRequest) -> func.HttpResponse:
-    logging.warning("Received Interaction from Discord")
+@app.route(route="interactions", auth_level=func.AuthLevel.ANONYMOUS)
+def handle_request(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info("Received Interaction from Discord")
         
     try:
         verify_request(req)
         
         body = req.get_json()
-        
-        logging.warning(body)
-        
+                
         if body["type"] == 1:
-            logging.warning("Got ping request")
-            return func.HttpResponse(
-                json.dumps({
-                    "type": 1
-                    })
-            )
+            logging.info("Got ping request")
+            return json_response({"type": 1})
+
         elif body["type"] == 2:
-            logging.warning("Incoming Application Command")
+            logging.info("Incoming Application Command")
             return command_handler(body)
         
         #Unhandled message type
         logging.error("Unhandled message type")
-        return func.HttpResponse(
-                json.dumps({
-                    "type": 1 
-                    }),
-            )
+        return json_response({"type": 1})
+
         
     except BadSignatureError:
         logging.error("Failed to verify signature")
-        return func.HttpResponse(
-            json.dumps({
+
+        return json_response({
                 "Error": "Could not verify interaction signature"
-                }),
-            status_code = 401
+            }, status_code=401
         )
     except Exception as ex:
         logging.error(ex)
-        return func.HttpResponse(
-            json.dumps({
-                "Unkown Error": ex 
-                }),
-            status_code = 500
+
+        return json_response({
+            "Unkown Error": ex
+            }, status_code=500
         )
 
 def command_handler(body) -> func.HttpResponse:
-    return func.HttpResponse(
-        json.dumps({
-            "type": 2,
+    return json_response({
+            "type": 4,
             "data": {
                 "content": "Hello, world!"
             }
-        })
+        }
+    )
+
+def json_response(json, status_code = 200) -> func.HttpResponse:
+    return func.HttpResponse(
+        json.dumps(json),
+        status_code=status_code,
+        mimetype="application/json"
     )
 
 def verify_request(req: func.HttpRequest):
